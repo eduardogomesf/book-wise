3b0f7ef5c9cfec3fd09d9cc94f86003dfa349ca6
eduardogomesf <eduardogoomesf@gmail.com>
2023-07-28 01:08:30 -0300

    feat: retrieve categories in SSR and add selection logic

--- src/pages/explore/index.page.tsx
+++ src/pages/explore/index.page.tsx
@@ -1,9 +1,38 @@
-import { ReactElement } from "react"
+import { ReactElement, useState } from "react"
 import { DefaultLayout } from "../../layouts/default"
 import { ExplorerContainer, Header, InputContainer, Tag, TagsContainer, TitleContainer } from "./styles"
 import { Binoculars, MagnifyingGlass } from "phosphor-react"
+import { GetServerSideProps } from "next"
+import { Category } from "../../types/category"
+import { api } from "../../lib/axios"
 
-export default function Explore() {
+type ExploreProps = {
+  categories: Category[]
+}
+export default function Explore({ categories = [] }: ExploreProps) {
+  const [selectedCategories, setSelectedCategories] = useState<string[]>(['All'])
+
+  function handleSelectCategory(categoryId: string) {
+    const categoryIsAlreadySelected = selectedCategories.includes(categoryId)
+    const numberOfSelectedCategories = selectedCategories.length
+
+    if (categoryIsAlreadySelected) {
+      if (numberOfSelectedCategories === 1) {
+        setSelectedCategories(['All'])
+        return
+      }
+
+      setSelectedCategories(selectedCategories.filter(category => category !== categoryId))
+    } else {
+      const oldCategories = selectedCategories.filter(category => category !== 'All')
+      setSelectedCategories([...oldCategories, categoryId])
+    }
+  }
+
+  function resetCategories() {
+    setSelectedCategories(['All'])
+  }
+
   return (
     <ExplorerContainer>
       <Header>
@@ -19,15 +48,22 @@ export default function Explore() {
       </Header>
 
       <TagsContainer>
-        <Tag selected={true}>All</Tag>
+        <Tag
+          selected={selectedCategories.includes('All')}
+          onClick={() => resetCategories()}
+        >
+          All
+        </Tag>
 
-        <Tag selected={false}>Computing</Tag>
-
-        <Tag selected={false}>Education</Tag>
-
-        <Tag selected={false}>Fictitious</Tag>
-
-        <Tag selected={false}>Science Fiction</Tag>
+        {categories.slice(0, 7).map(category => (
+          <Tag
+            key={category.id}
+            onClick={() => handleSelectCategory(category.id)}
+            selected={selectedCategories.includes(category.id)}
+          >
+            {category.name}
+          </Tag>
+        ))}
       </TagsContainer>
     </ExplorerContainer>
   )
@@ -39,4 +75,16 @@ Explore.getLayout = function getLayout(page: ReactElement) {
       {page}
     </DefaultLayout>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const getCategoriesResponse = await api.get("http://localhost:3000/api/categories")
+
+  const categories = getCategoriesResponse.data.categories
+
+  return {
+    props: {
+      categories
+    }
+  }
 }
